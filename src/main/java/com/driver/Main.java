b57891f9faee379f61c1cd0f30f1f7749206831e
Satish <satishkrgupta555@gmail.com>
2023-01-13 22:28:58 +0530

    method_overriding v1.0

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,27 @@
 package com.driver;
+import java.io.*;
+import java.lang.*;
+import java.sql.SQLOutput;
+import java.util.*;
+import org.w3c.dom.ls.LSOutput;
 
 public class Main {
+    public static class A{
+        public static String meth(){
+            return "Invoking method from class A";
+        }
+    }
+
+    public static class B extends A{
+//        public static String meth(){
+//            return "Method is overridden in Extendend class B";
+//        }
+    }
+
+    public static void main(String[] args){
+        B newB = new B();
+        System.out.println(newB.meth());
+    }
+
   
 }
